42698b2b12b84d9da661b66ffa4ff354dc1a8658
Abdulaziz-learner <abdulazizkutbidinov@gmail.com>
2022-04-13 18:29:35 +0500

    userslist added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import UsersList from "./User/UsersList";
 import AddUser from "./User/AddUser";
 function App() {
   return (
     <div className="App">
      <AddUser/>
+     <UsersList users={[]}/>
     </div>
   );
 }
