59bb514929ccdd3884942513f7cd42284d88503d
Abdulaziz-learner <abdulazizkutbidinov@gmail.com>
2022-04-14 06:15:27 +0500

    userslist committed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import UsersList from "./User/UsersList";
+import React, {useState} from "react";
 import AddUser from "./User/AddUser";
 function App() {
+ const [usersList,setUsersList] = useState([]);
+ const addUserHandler = (uName,uAge)=>{
+   setUsersList((prevUsersList)=>{
+     return [...prevUsersList,{name:uName,age:uAge}]
+   })
+ }
   return (
     <div className="App">
-     <AddUser/>
+     <AddUser onAddUser={addUserHandler}/>
      <UsersList users={[]}/>
     </div>
   );
